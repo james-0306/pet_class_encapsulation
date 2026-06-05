025a43cc11b461aade065cc985eec0877337a82f
james malitan <jamesmalitan21@gmail.com>
2026-06-05 14:19:44 +0800

    making a setter method to set the attributes

--- pet_simulator.py
+++ pet_simulator.py
@@ -4,3 +4,12 @@ class Pet:
         self.__animal_type = ""
         self.__age = 0
 
+    # setter method
+    def set_name(self, name):
+        self.__name = name
+
+    def set_animal_type(self, animal_type):
+        self.__animal_type = animal_type
+
+    def set_age(self, age):
+        self.__age = age
